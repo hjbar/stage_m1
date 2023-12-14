a503674049ee52a5194e5775e815904a413c8731
Gabriel Scherer <gabriel.scherer@gmail.com>
2023-12-14 14:00:24 +0100

    a sampling RandGen
    fixup randgen

--- src/RandomGenerator.ml
+++ src/RandomGenerator.ml
@@ -1,20 +1,65 @@
+(*
 module RandGen = struct
   type 'a t = 'a Seq.t
   let map = Seq.map
 
   let pure x = Seq.return x
   let pair sa sb = Seq.product sa sb
-
-  let fail = Seq.empty
-  let plus = Seq.append
-
+     
+  let sum seqs = seqs |> List.to_seq |> Seq.concat
   let bind = Seq.concat_map
 end
 module _ = (RandGen : Utils.Applicative)
+*)
+
+let shuffle arr =
+  for i = Array.length arr - 1 downto 1 do
+    let j = Random.int (i + 1) in
+    let t = arr.(j) in
+    arr.(j) <- arr.(i);
+    arr.(i) <- t
+  done
+
+module RandGen = struct
+  type 'a t = unit -> 'a option
+  let map f v = fun () -> Option.map f (v ())
+  let pure x : 'a t = fun () -> Some x
+  let pair v1 v2 : ('a * 'b) t = fun () ->
+    match v1 (), v2 () with
+    | Some a1, Some a2 -> Some (a1, a2)
+    | None, _ | _, None -> None
+
+  let fail = fun () -> None
+
+  let one_of (arr : 'a array) : 'a t =
+    fun () ->
+    if arr = [| |] then None
+    else Some arr.(Random.int (Array.length arr))
+
+
+  let sum (li : 'a t list) : 'a t =
+    let choices = Array.of_list li in
+    fun () ->
+      shuffle choices;
+      Array.find_map (fun f -> f ()) choices
+
+  let delay (f : unit -> 'a t) : 'a t =
+    fun () -> f () ()
+
+  let bind f v =
+    fun () ->
+    match v () with
+    | None -> None
+    | Some a -> f a ()
+
+  let run ~limit (gen : 'a t) : 'a list =
+    Seq.forever (fun () -> gen)
+    |> Seq.filter_map (fun f -> f ())
+    |> Seq.take limit
+    |> List.of_seq
+end
 
 let ret = RandGen.pure
-let fail = RandGen.fail
-let (++) = RandGen.plus
 let (let+) s f = RandGen.map f s
 let (and+) sa sb = RandGen.pair sa sb
 let ( let* ) s f = RandGen.bind f s
@@ -44,15 +89,22 @@ let untyped : Untyped.term =
     Utils.namegen Var.fresh
       [|"x"; "y"; "z"; "u"; "v"; "w"|]
   in
-  let rec gen env = Do (fun () -> () |>
-    (let+ x = TeVarSet.to_seq env.Env.tevars in Var x)
-    ++
-    ret (App(gen env, gen env))
-    ++
-    ret (
-      let x = new_var () in
-      Abs(x, gen (Env.bind_tevar x env))
-    )
+  let rec gen env =
+    let fvars =
+      env.Env.tevars
+      |> TeVarSet.to_seq
+      |> Array.of_seq
+      |> RandGen.one_of
+    in
+    Do (fun () -> () |>
+    RandGen.sum [
+      (let+ x = fvars in Var x);
+      RandGen.pure (App(gen env, gen env));
+      (fun () ->
+        let x = new_var () in
+        Some (Abs(x, gen (Env.bind_tevar x env)))
+      );
+    ]
   ) in
   gen (Env.empty ())
 
@@ -79,13 +131,13 @@ let typed ~depth : STLC.term RandGen.t =
   end in
   let rec loop : type a e r . fuel:int -> env -> (a, e) Constraint.t -> a RandGen.t =
   fun ~fuel env cstr ->
-    if fuel = 0 then fail else
+    if fuel = 0 then RandGen.fail else
     let _logs, env, cstr =
       ConstraintSolver.eval ~log:false env cstr
     in
     match cstr with
     | Ret v -> ret (v (Decode.decode env))
-    | Err _ -> fail
+    | Err _ -> RandGen.fail
     | _ ->
     let open Constraint in
     let ( let+ ) g f = RandGen.map f g in
