a19fe0635cb5b0bf7c993204e615c1f25e90bedc
Gabriel Scherer <gabriel.scherer@gmail.com>
2023-12-21 00:25:34 +0100

    improve extraction scripts for public release

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -e
 
-if [ ! -d $DIR/sujet ]
+TEST_FILE=$DIR/constrained_generation.opam
+if [ ! -f $TEST_FILE ]
 then
-    echo "Error: No directory $DIR/sujet (\$DIR/sujet)."
+    echo "Error: No file $TEST_FILE (\$DIR/constrained_generation.opam)."
     echo "We are probably not at the right place, abort."
     exit 2
 fi
 
+if [ ! -d sujet ]
+then
+    echo "Error: no sujet/ directory."
+    echo "You should run scripts/extract.sh first."
+    exit 3
+fi
+
 set -x
 
-rm -fR $DIR/sujet
-mv sujet/ $DIR/
-
+rm -fR $DIR/*
+mv sujet/* $DIR/
+rmdir sujet
