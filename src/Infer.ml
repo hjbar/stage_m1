2e5d2219a8be21d7ec4261f926f4f846aaf84648
Neven Villani <vanille@crans.org>
2023-12-26 18:34:12 +0100

    fmt

--- src/Infer.ml
+++ src/Infer.ml
@@ -89,28 +89,31 @@ module Make(T : Utils.Functor) = struct
       Precondition: when calling [has_type env t], [env] must map each
       term variable that is free in [t] to an inference variable.
   *)
-  let rec has_type (env : env) (t : Untyped.term) (w : variable) : (STLC.term, err) t =
-    match t with
-    | Untyped.Var x ->
+    let rec has_type (env : env) (t : Untyped.term) (w : variable) : (STLC.term, err) t =
+        let fresh = Constraint.Var.fresh in
+        let fresh_from x = fresh (Untyped.Var.name x) in
+        match t with
+        | Untyped.Var x -> (
             let x_inference = STLC.TeVar.Map.find x env in
             let+ () = eq w x_inference
             in STLC.Var x
-    | Untyped.App (t, u) ->
-            let t_var = Constraint.Var.fresh "wt"
-            and u_var = Constraint.Var.fresh "wu" in
-            Exist (u_var, None,
+        )
+        | Untyped.App (t, u) -> (
+            let t_var = fresh "wt"
+            and u_var = fresh "wu"
+            in Exist (u_var, None,
                 Exist (t_var, Some (Arrow (u_var, w)),
                     let+ t_typed = has_type env t t_var
                     and+ u_typed = has_type env u u_var
                     in STLC.(App (t_typed, u_typed))
                 )
             )
-    | Untyped.Abs (x, t) ->
-            let x_var = Constraint.Var.fresh (Untyped.Var.name x)
-            and t_var = Constraint.Var.fresh "warr"
-            and res_var = Constraint.Var.fresh "wt"
-            in
-            Exist (x_var, None,
+        )
+        | Untyped.Abs (x, t) -> (
+            let x_var = fresh_from x
+            and t_var = fresh "warr"
+            and res_var = fresh "wt"
+            in Exist (x_var, None,
                 Exist (res_var, None,
                     Exist (t_var, Some (Arrow (x_var, res_var)),
                         let env' = STLC.TeVar.Map.(env |> add x x_var) in
@@ -121,10 +124,11 @@ module Make(T : Utils.Functor) = struct
                     )
                 )
             )
-    | Untyped.Let (x, t, u) ->
-            let t_var = Constraint.Var.fresh (Untyped.Var.name x)
-            and u_var = Constraint.Var.fresh "u" in
-            Exist (t_var, None,
+        )
+        | Untyped.Let (x, t, u) -> (
+            let t_var = fresh_from x
+            and u_var = fresh "u"
+            in Exist (t_var, None,
                 Exist (u_var, None,
                     let env' = STLC.TeVar.Map.(env |> add x t_var) in
                     let+ let_typ = decode t_var
@@ -133,49 +137,52 @@ module Make(T : Utils.Functor) = struct
                     in STLC.(Let (x, let_typ, t_typed, u_typed))
                 )
             )
-    | Untyped.Annot (t, ty) ->
+        )
+        | Untyped.Annot (t, ty) -> (
             bind ty (fun ty_var ->
                 let+ t_typed = has_type env t ty_var
                 and+ () = eq ty_var w
                 in t_typed
             )
-    | Untyped.Tuple ts ->
-        let (t1, t2) = assume_pair ts
-        and t1_var = Constraint.Var.fresh "w1"
-        and t2_var = Constraint.Var.fresh "w2"
-        and prod_var = Constraint.Var.fresh "prod"
-        in
-        Exist (t1_var, None,
-            Exist (t2_var, None,
-                Exist (prod_var, Some (Prod [t1_var; t2_var]),
-                    let+ t1_typed = has_type env t1 t1_var
-                    and+ t2_typed = has_type env t2 t2_var
-                    and+ () = eq w prod_var
-                    in STLC.(Tuple [t1_typed; t2_typed])
+        )
+        | Untyped.Tuple ts -> (
+            let (t1, t2) = assume_pair ts
+            and t1_var = fresh "w1"
+            and t2_var = fresh "w2"
+            and prod_var = fresh "prod"
+            in Exist (t1_var, None,
+                Exist (t2_var, None,
+                    Exist (prod_var, Some (Prod [t1_var; t2_var]),
+                        let+ t1_typed = has_type env t1 t1_var
+                        and+ t2_typed = has_type env t2 t2_var
+                        and+ () = eq w prod_var
+                        in STLC.(Tuple [t1_typed; t2_typed])
+                    )
                 )
             )
         )
-    | Untyped.LetTuple (xs, t, u) ->
-        let (x1, x2) = assume_pair xs in
-        let x1_var = Constraint.Var.fresh (Untyped.Var.name x1)
-        and x2_var = Constraint.Var.fresh (Untyped.Var.name x2)
-        and prod_var = Constraint.Var.fresh "wt"
-        in
-        Exist (x1_var, None,
-            Exist (x2_var, None,
-                Exist (prod_var, Some (Prod [x1_var; x2_var]),
-                    let env' = STLC.TeVar.Map.(env |> add x1 x1_var |> add x2 x2_var) in
-                    let+ x1_typ = decode x1_var
-                    and+ x2_typ = decode x2_var
-                    and+ t_typed = has_type env t prod_var
-                    and+ u_typed = has_type env' u w
-                    in STLC.LetTuple ([(x1, x1_typ); (x2, x2_typ)], t_typed, u_typed)
+        | Untyped.LetTuple (xs, t, u) -> (
+            let (x1, x2) = assume_pair xs in
+            let x1_var = fresh_from x1
+            and x2_var = fresh_from x2
+            and prod_var = fresh "wt"
+            in Exist (x1_var, None,
+                Exist (x2_var, None,
+                    Exist (prod_var, Some (Prod [x1_var; x2_var]),
+                        let env' = STLC.TeVar.Map.(env |> add x1 x1_var |> add x2 x2_var) in
+                        let+ x1_typ = decode x1_var
+                        and+ x2_typ = decode x2_var
+                        and+ t_typed = has_type env t prod_var
+                        and+ u_typed = has_type env' u w
+                        in STLC.LetTuple ([(x1, x1_typ); (x2, x2_typ)], t_typed, u_typed)
+                    )
                 )
             )
         )
-    | Do p ->
-      (* Feel free to postone this until you start looking
-         at random generation. Getting type inference to
-         work on all the other cases is a good first step. *)
-      Utils.not_yet "Infer.has_type: Do case" (env, p, fun () -> has_type)
+        | Do p -> (
+          (* Feel free to postone this until you start looking
+             at random generation. Getting type inference to
+             work on all the other cases is a good first step. *)
+            Utils.not_yet "Infer.has_type: Do case" (env, p, fun () -> has_type)
+        )
 end
