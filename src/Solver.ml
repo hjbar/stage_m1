209a364d75794661b7fcd4d48a55d71e30952340
hjbar <hjbar@protonmail.com>
2025-05-16 18:33:00 +0200

    Try to add assoc_env to fix, but failed

--- src/Solver.ml
+++ src/Solver.ml
@@ -16,6 +16,10 @@ module Make (T : Utils.Functor) = struct
 
   type solver_env = Generalization.scheme SEnv.t
 
+  module Assoc = Map.Make (Constraint.Var)
+
+  type assoc_env = Constraint.Var.t Assoc.t
+
   type log = PPrint.document list
 
   let make_logger c0 =
@@ -62,13 +66,14 @@ module Make (T : Utils.Functor) = struct
 
          (You can also tweak this code temporarily to print stuff on
          stderr right away if you need dirtier ways to debug.)
-      *)
+    *)
     let add_to_log, get_log =
       if log then make_logger c0 else (ignore, fun _ -> [])
     in
 
     let solver_env : solver_env ref = ref SEnv.empty in
     let unif_env : unif_env ref = ref unif_env in
+    let assoc_env : assoc_env ref = ref Assoc.empty in
 
     let rec eval : type a e. (a, e) Constraint.t -> (a, e) normal_constraint =
       let open Constraint in
@@ -98,27 +103,34 @@ module Make (T : Utils.Functor) = struct
           MapErr (c, f)
       end
       | Conj (c, d) -> begin
-        match (eval c, eval d) with
-        | NRet v, NRet w -> nret @@ fun sol -> (v sol, w sol)
-        | NErr e, _ | _, NErr e -> NErr e
-        | NDo p, NDo q ->
-          ndo
-          @@
-          let+ c = p in
-          Conj (c, Do q)
-        | NDo p, NRet w ->
-          ndo
-          @@
-          let+ c = p in
-          Conj (c, Ret w)
-        | NRet v, NDo q ->
-          ndo
-          @@
-          let+ d = q in
-          Conj (Ret v, d)
+        match eval c with
+        | NErr e -> NErr e
+        | nc -> begin
+          match (nc, eval d) with
+          | NErr e, _ | _, NErr e -> NErr e
+          | NRet v, NRet w -> nret @@ fun sol -> (v sol, w sol)
+          | NRet v, NDo q ->
+            ndo
+            @@
+            let+ d = q in
+            Conj (Ret v, d)
+          | NDo p, NRet w ->
+            ndo
+            @@
+            let+ c = p in
+            Conj (c, Ret w)
+          | NDo p, NDo q ->
+            ndo
+            @@
+            let+ c = p in
+            Conj (c, Do q)
+        end
       end
       | Eq (x1, x2) -> begin
-        match Unif.unify !unif_env x1 x2 with
+        match
+          Unif.unify !unif_env (Assoc.find x1 !assoc_env)
+            (Assoc.find x2 !assoc_env)
+        with
         | Ok new_env ->
           unif_env := new_env;
           add_to_log !unif_env;
@@ -137,9 +149,12 @@ module Make (T : Utils.Functor) = struct
           but reuse the previous binding which accumulated
           information through unifications.
         *)
-        if not @@ Unif.Env.mem x !unif_env then begin
-          unif_env := Unif.Env.add x s !unif_env;
-          add_to_log !unif_env
+        if not @@ Assoc.mem x !assoc_env then begin
+          let new_unif_env, uvar = Generalization.flexible None !unif_env in
+          unif_env := new_unif_env;
+          add_to_log !unif_env;
+
+          assoc_env := Assoc.add x uvar !assoc_env
         end;
 
         match eval c with
@@ -154,13 +169,17 @@ module Make (T : Utils.Functor) = struct
       | Decode v -> nret @@ fun sol -> sol v
       | Do p -> NDo p
       | DecodeScheme sch_var ->
-        ignore sch_var;
-        failwith "Solver.eval.DecodeScheme TODO"
+        let scheme = SEnv.find sch_var !solver_env in
+
+        let body = Generalization.body scheme in
+        let quantifiers = Generalization.quantifiers scheme in
+
+        nret @@ fun sol -> (quantifiers, sol body)
       | Instance (sch_var, w) -> begin
         let sch = SEnv.find sch_var !solver_env in
         let witnesses, var = Generalization.instantiate sch !unif_env in
 
-        match Unif.unify !unif_env w var with
+        match Unif.unify !unif_env (Assoc.find w !assoc_env) var with
         | Ok new_env ->
           unif_env := new_env;
           add_to_log !unif_env;
@@ -174,23 +193,21 @@ module Make (T : Utils.Functor) = struct
       | Let (sch_var, var, c1, c2) -> begin
         unif_env := Generalization.enter !unif_env;
 
-        let new_unif_env, uvar = Generalization.flexible None !unif_env in
-        unif_env := new_unif_env;
-        add_to_log !unif_env;
+        if not @@ Assoc.mem var !assoc_env then begin
+          let new_unif_env, uvar = Generalization.flexible None !unif_env in
+          unif_env := new_unif_env;
+          add_to_log !unif_env;
 
-        if not @@ Unif.Env.mem var !unif_env then begin
-          unif_env := Unif.Env.add var None !unif_env;
-          add_to_log !unif_env
+          assoc_env := Assoc.add var uvar !assoc_env
         end;
 
         match eval c1 with
         | NRet r1 -> begin
-          let new_unif_env, gammas, schemes =
-            Generalization.exit [ uvar ] !unif_env
+          let new_unif_env, _gammas, schemes =
+            Generalization.exit [ Assoc.find var !assoc_env ] !unif_env
           in
           unif_env := new_unif_env;
 
-          assert (List.length gammas = 1);
           assert (List.length schemes = 1);
 
           solver_env := SEnv.add sch_var (List.hd schemes) !solver_env;
