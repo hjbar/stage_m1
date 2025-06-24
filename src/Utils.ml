8332cb3f13244ba2485358c56616d41c1541f59c
hjbar <hjbar@protonmail.com>
2025-06-24 14:34:29 +0200

    Add a file Utils.mli

--- src/Utils.ml
+++ src/Utils.ml
@@ -10,28 +10,7 @@ let string_of_doc doc =
   Buffer.contents buf
 
 
-module Variables () : sig
-  type t = private {
-    name : string;
-    stamp : int;
-  }
-
-  val compare : t -> t -> int
-
-  val eq : t -> t -> bool
-
-  val fresh : string -> t
-
-  val namegen : string array -> unit -> t
-
-  val name : t -> string
-
-  val print : t -> PPrint.document
-
-  module Set : Set.S with type elt = t
-
-  module Map : Map.S with type key = t
-end = struct
+module Variables () = struct
   type t = {
     name : string;
     stamp : int;
@@ -46,7 +25,7 @@ end = struct
 
   let eq n1 n2 = compare n1 n2 = 0
 
-  let stamps = Hashtbl.create 42
+  let stamps = Hashtbl.create 16
 
   let fresh name =
     let stamp =
@@ -60,8 +39,10 @@ end = struct
 
   let namegen names =
     if names = [||] then failwith "namegen: empty names array";
+
     let counter = ref 0 in
     let wrap n = n mod Array.length names in
+
     fun () ->
       let idx = !counter in
       counter := wrap (!counter + 1);
@@ -89,8 +70,6 @@ module type Functor = sig
   val map : ('a -> 'b) -> 'a t -> 'b t
 end
 
-(** A signature for search monads, that represent computations that enumerate
-    zero, one or several values. *)
 module type MonadPlus = sig
   include Functor
 
@@ -102,33 +81,16 @@ module type MonadPlus = sig
 
   val fail : 'a t
 
-  (** [fail] and [one_of] can be derived from [sum], but they typically have
-      simpler and more efficient specialized implementations. *)
   val one_of : 'a array -> 'a t
 
-  (** Many search monad implementations perform their computation on-demand,
-      when elements are requested, instead of forcing computation already to
-      produce the ['a t] value.
-
-      In a strict language, it is easy to perform computation too early in this
-      case, for example [M.sum [foo; bar]] will compute [foo] and [bar] eagerly
-      even though [bar] may not be needed if we only observe the first element.
-
-      The [delay] combinator makes this on-demand nature explicit, for example
-      one can write [M.delay (fun () -> M.sum [foo; bar])] to avoid computing
-      [foo] and [bar] too early. Of course, if the underlying implementation is
-      in fact eager, then this may apply the function right away.*)
   val delay : (unit -> 'a t) -> 'a t
 
-  (** ['a Seq.t] is a type of on-demand sequences from the OCaml standard
-      library: https://v2.ocaml.org/api/Seq.html *)
   val run : 'a t -> 'a Seq.t
 end
 
 module Empty = struct
   type 'a t = |
 
-  (* the empty type *)
   let map (_ : 'a -> 'b) : 'a t -> 'b t = function
     | _ -> .
 end
